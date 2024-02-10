fe3c396988e4dc903f5bf3975ebfbd36efade6e3
emmanuelterah <emmanuelterah98@gmail.com>
2024-02-11 01:10:33 +0300

    Done with the code-along

---
+++ src/Components/NavBar.js
@@ -0,0 +1,20 @@
+export default function NavBar(){
+    return(
+      <div>
+        <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{background:"darkblue",}}
+        >
+          Home
+        </NavLink>
+        <NavLink to="/about" exact style={linkStyles} activeStyle={{background:"darkblue"}}>
+          About
+        </NavLink>
+        <NavLink to="/login" exact style={linkStyles} activeStyle={{background: "darkblue"}}>
+          Login
+        </NavLink>
+      </div>
+    )
+  }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
+/* Add basic styling for NavLinks */
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+// Define the NAvBAr component
+
+function NavBar(){
+  return(
+    <div>
+      <NavLink
+      to="/"
+      exact
+      style={linkStyles}
+      activeStyle={{background:"darkblue",}}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about" exact style={linkStyles} activeStyle={{background:"darkblue"}}>
+        About
+      </NavLink>
+      <NavLink to="/login" exact style={linkStyles} activeStyle={{background: "darkblue"}}>
+        Login
+      </NavLink>
+    </div>
+  )
+}
 
 function Home() {
   return (
@@ -9,8 +44,46 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+  <NavBar />
+  <Switch>
+    
+    <Route path="/about">
+      <About/>
+    </Route>
+    <Route path="/login">
+      <Login/>
+    </Route>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
